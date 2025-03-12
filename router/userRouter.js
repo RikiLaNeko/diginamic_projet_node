7d293fde1412b5c9ec2efaa67da853a10f4ea502
wsem-sys <wsem@outlook.fr>
2025-03-12 16:52:14 +0100

    auth jwt maj

---
+++ router/userRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+
+
+// POST /users/register => Inscription d'un utilisateur
+router.post('/register', userController.register);
+
+// POST /users/login => Connexion d'un utilisateur
+router.post('/login', userController.login);
+
+module.exports = router;
+
