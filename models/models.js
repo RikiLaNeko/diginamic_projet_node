ad8254111c889a78f66dabbb58ed8b65693b051a
RikiLaNeko <arthur.mortreux@lyceeastier.com>
2025-03-14 10:43:29 +0100

    feat: add User model and user router for bar management

--- models/models.js
+++ models/models.js
@@ -2,6 +2,7 @@ const Bars = require('./bars');
 const Biere = require('./biere');
 const BiereCommande = require('./biere_commande');
 const Commande = require('./commande');
+const User = require('./User');
 
 Bars.hasMany(Biere, { foreignKey: 'bars_id', onDelete: 'CASCADE' });
 Biere.belongsTo(Bars, { foreignKey: 'bars_id' });
@@ -12,9 +13,14 @@ Commande.belongsTo(Bars, { foreignKey: 'bars_id' });
 Biere.belongsToMany(Commande, { through: BiereCommande, foreignKey: 'biere_id' });
 Commande.belongsToMany(Biere, { through: BiereCommande, foreignKey: 'commande_id' });
 
+User.belongsTo(Bars, { foreignKey: 'bar_id' });
+Bars.hasMany(User, { foreignKey: 'bar_id', onDelete: 'CASCADE' });
+
+
 module.exports = {
     Bars,
     Biere,
     BiereCommande,
-    Commande
+    Commande,
+    User
 };
--- router/routers.js
+++ router/routers.js
@@ -3,6 +3,7 @@ const barsRouter = require('./barsRouter');
 const biereRouter = require('./biereRouter');
 const commandeRouter = require('./commandeRouter');
 const biereCommandeRouter = require('./biere_commandeRouter');
+const userRouter = require('./userRouter');
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/bars', barsRouter);
 router.use('/biere', biereRouter);
 router.use('/commandes', commandeRouter);
 router.use('/biere_commande', biereCommandeRouter);
+router.use('/users', userRouter);
 
 module.exports = router;
